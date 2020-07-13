0587c1c16109b4f27c14407c858196d2dc72a3cd
aswinjayaji <56126732+aswinjayaji@users.noreply.github.com>
2020-07-13 23:59:16 +0530

    Add files via upload

---
+++ webscrapping.py
@@ -0,0 +1,12 @@
+import urllib.request, urllib.parse, urllib.error
+import ssl
+
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+url = input('Enter - ')
+html = urllib.request.urlopen(url, context=ctx).decode().read()
+fhand=open("file.html","a")
+fhand.write(html)
+
